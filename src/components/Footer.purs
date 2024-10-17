7340c7b419316fe686ff6dbbb07c93759ba80c05
nkarl <21993921+nkarl@users.noreply.github.com>
2024-10-16 22:57:01 -0700

    Laid out some basic frame for the home page.

--- src/components/Footer.purs
+++ src/components/Footer.purs
@@ -1,10 +1,12 @@
 module Component.Footer where
 
 import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 
 footer :: forall i p. HH.HTML i p
 footer =
-  HH.footer_
+  HH.footer
+    [ HP.style "padding:1em; border:solid black 1px; text-align:center" ]
     [ HH.div_
       [ HH.text "© 2024 Charles Lambert Nguyen. All rights reserved."
       ]
--- src/components/Header.purs
+++ src/components/Header.purs
@@ -5,22 +5,31 @@ import Halogen.HTML.Properties as HP
 
 header :: forall i p. HH.HTML i p
 header =
-  HH.footer_
-    [ HH.div_
-        [ HH.text "Hello, welcome to my blog!"
-        ]
-    , HH.ul_
-        [ HH.li_
+  HH.footer
+    [ HP.style "padding:0.25em; border:solid black 1px; display:flex; justify-content:space-between;" ]
+    [ HH.div
+        [ HP.style "align-items:space-around; display:inline-flex; justify-content:space-around;" ]
+        --[ HP.style "margin: 0 1em;align-items:center" ]
+        [ HH.div
+            [ HP.style "margin:0em; padding: 1em; border: dotted red 1px; height:48px; width:48px;" ]
             [ HH.a
-                [ HP.href "https://nkarl.github.io/" ]
-                [ HH.text "home" ]
+              [ HP.href "/" ]
+              [ HH.text "home" ]
             ]
-        , HH.li_
+        , HH.div
+            [ HP.style "margin:0em; padding: 1em" ]
+            [ HH.text "Hello, welcome to my blog!" ]
+        ]
+    , HH.ul
+        [ HP.style "margin: 0em; padding: 1em; align-items:space-around; display:inline-flex; justify-content:space-around;" ]
+        [ HH.li
+            [ HP.style "margin:0 1em" ]
             [ HH.a
                 [ HP.href "https://nkarl.github.io/resume" ]
                 [ HH.text "resume" ]
             ]
-        , HH.li_
+        , HH.li
+            [ HP.style "margin:0 1em" ]
             [ HH.a
                 [ HP.href "https://nkarl.github.io/contact" ]
                 [ HH.text "contact" ]
